d0ddc3e578ad304db760ed3a4556ce9c4527d413
SAHIL <nasrat_sahil04@yahoo.com>
2021-08-10 22:18:47 -0400

    afaefasdfda

---
+++ src/Selenium12/Demo24.java
@@ -0,0 +1,15 @@
+package Selenium12;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Demo24 {
+    public static void main(String[] args) {
+
+
+        System.setProperty("webdriver.chrome.driver", "drivers//chromedriver.exe");
+        WebDriver driver=new ChromeDriver();
+
+
+    }
+}
